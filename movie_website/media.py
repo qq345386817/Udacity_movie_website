df71aad059928b6ff125121297aa3b606fdefcff
LuoPK <luopeike0911@163.com>
2017-04-14 08:34:09 +0800

    First Commit

---
+++ movie_website/media.py
@@ -0,0 +1,19 @@
+import webbrowser
+
+class Video():
+    """docstring for video"""
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+
+class Movie(Video):
+    '''docstring for Movie'''
+    def __init__(self, title, duration, storyline, poster_image_url, trailer_youtube_url):
+        Video.__init__(self, title, duration)
+        self.storyline = storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
