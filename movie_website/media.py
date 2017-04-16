0e00d67569d2f11738758a71ab7b27d457f6dc42
LuoPK <luopeike0911@163.com>
2017-04-16 08:53:43 +0800

    add commit.

--- movie_website/media.py
+++ movie_website/media.py
@@ -1,14 +1,20 @@
 import webbrowser
 
+"""
+    Class Video is the super class of Movie.
+    Class Movie will store the information for each movie created in
+    entertainment center.
+"""
+
 class Video():
-    """docstring for video"""
+    """Video contains title and duration."""
     def __init__(self, title, duration):
         self.title = title
         self.duration = duration
 
 
 class Movie(Video):
-    '''docstring for Movie'''
+    '''Movie store the storyline, poster_image_url and trailer_youtube_url.'''
     def __init__(self, title, duration, storyline, poster_image_url, trailer_youtube_url):
         Video.__init__(self, title, duration)
         self.storyline = storyline
